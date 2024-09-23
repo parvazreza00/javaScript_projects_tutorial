5796ee056c938c1a0c9758e442d0579d060442e2
parvaz <parvazreza00@gmail.com>
2024-09-23 12:18:05 +0600

    javaScript_projects_tutorial

---
+++ projects/dom/index.js
@@ -0,0 +1,7 @@
+const message = document.querySelector('#message');
+const result = document.querySelector('#result');
+
+
+message.addEventListener('input', ()=>{
+    result.textContent = message.value;
+})
