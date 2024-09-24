b3c6b94056ae7dfa95c6d3083eb2354066ca2623
parvaz <parvazreza00@gmail.com>
2024-09-24 18:02:25 +0600

    updated

---
+++ projects/dom/dom1/index.js
@@ -0,0 +1,12 @@
+const makeColor = document.getElementById('makeColor');
+makeColor.onclick = makeColored;
+function makeColored(){
+    const divtag = document.getElementById('divtag');
+    divtag.style.backgroundColor = 'green';
+}
+
+
+
+
+
+
