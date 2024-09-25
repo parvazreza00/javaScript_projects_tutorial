106a9707e64627e2d90c57c210e9f4305fb01a36
parvaz <parvazreza00@gmail.com>
2024-09-25 16:17:24 +0600

    Api response

--- projects/dom/dom1/index.js
+++ projects/dom/dom1/index.js
@@ -1,14 +1,36 @@
-const makeColor = document.getElementById('makeColor');
-makeColor.addEventListener('click', function makeColored(){
-    document.body.style.backgroundColor = "purple";
-});
+function loadApiData() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+    .then((data) => displayData(data));
+}
 
-// function makeColored(){
-//     document.body.style.backgroundColor = "purple";
-// }
+function displayData(data){
+ console.log(data);
+  const divTag = document.getElementById('divTag');
 
+  // for(let user of data){
+  //   const p = document.createElement('p');
+  //   p.innerText = user.username+" - "+user.phone+" - "+user.email;
+  //   divTag.appendChild(p);
+  // }
 
+  // for(let i = 0; i<data.length; i++){
+  //   const user = data[i];
+  //   const p = document.createElement('p');
+  //   p.innerText = user.username+" - "+user.phone+" - "+user.email; 
+  //   divTag.appendChild(p);
+  // }
 
+  data.forEach(users => {
+      const p = document.createElement('p');
+      p.innerText = users.username+" - "+users.phone+" - "+users.email;
+      divTag.appendChild(p);
+  })
 
+  // data.map(users => {
+  //   const p = document.createElement('p');
+  //   p.innerText = users.username+" - "+users.phone+" - "+users.email;
+  //   divTag.appendChild(p);
+  // });
 
-
+}
