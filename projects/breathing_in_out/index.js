537bad81320710a02027a125a541ec2fe42812de
parvaz <parvazreza00@gmail.com>
2024-10-07 00:34:02 +0600

    breathing in out complete

--- projects/breathing_in_out/index.js
+++ projects/breathing_in_out/index.js
@@ -1,16 +1,49 @@
+let timeLeft;
+let timerInterval;
+let isRunning = false;
+
 const timerDisplay = document.getElementById('timer');
 const btnStart = document.getElementById('btnStart');
 const btnEnd = document.getElementById('btnEnd');
 
-function startTimer(){
-    setInterval(() => {
-        console.log("hello");
+function startTimer(duration){
+    let timer = duration;
+
+    timerInterval = setInterval(() => {
+        const minutes = parseInt(timer / 60 , 10);
+        const seconds = parseInt(timer % 60, 10);
+
+        const displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+        const displaySeconds = seconds < 10 ? '0' + seconds : seconds;
+
+        timerDisplay.textContent = displayMinutes + ":" + displaySeconds;
+
+        if(--timer < 0){
+            clearInterval(timerInterval);
+            timerDisplay.textContent = 'Breath Out';
+
+            setTimeout(() => {
+                timerDisplay.textContent = 'Breath In';
+                startTimer(timeLeft);
+            }, 3000);
+        }
     }, 1000);
 }
 
+function timerStop(){
+    clearInterval(timerInterval);
+    timerDisplay.textContent = 'Breath In';
+    isRunning = false;
+}
+
 btnStart.addEventListener('click', ()=>{
-    startTimer();
+   if(!isRunning){
+    timeLeft = 15;
+    startTimer(timeLeft);
+    isRunning = 'true';
+   }
 });
 btnEnd.addEventListener('click', ()=>{
-    console.log("End");
+    timerStop();
+    isRunning = false;
 });
