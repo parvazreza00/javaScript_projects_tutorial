439ab2719b87fc52a4e7646c618676729da15bd8
parvazreza00 <parvazreza00@gmail.com>
2024-11-22 22:17:42 +0600

    js OOP

---
+++ problems/js_OOP/js_object.js
@@ -0,0 +1,11 @@
+const person = {
+    first_name: "Yeasin",
+    last_name: "Ahmed",
+    age: 28,
+    isBangladeshi: true,
+    getName:()=>{
+        return `My name is ${person.first_name} ${person.last_name}`
+    }
+}
+// console.log(person);
+console.log(person.getName());
