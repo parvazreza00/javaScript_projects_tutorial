bebe88d4f03a2b45c38c500502423e523e43ad37
parvazreza00 <parvazreza00@gmail.com>
2024-11-26 18:18:59 +0600

    js_es6

---
+++ problems/js_ES6/js_string.js
@@ -0,0 +1,3 @@
+var str = "Hello world";
+console.log(str.charAt(0));
+console.log(str[0]);
---
+++ problems/js_ES6/js_use_strict.js
@@ -0,0 +1,16 @@
+"use strict";
+var x = 2.00;
+console.log(x);
+
+function  myFunction() {
+    let name = "Parvaz Reza";
+    return name;
+}
+console.log(myFunction());
+
+function  valueFunction() {
+    "use strict";
+    let y = 222;
+    return y;
+}
+console.log(valueFunction());
