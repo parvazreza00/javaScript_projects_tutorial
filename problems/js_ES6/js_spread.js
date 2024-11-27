4f311fa8b261962b6af5f04f5eb2de191c31aa56
parvazreza00 <parvazreza00@gmail.com>
2024-11-28 00:48:03 +0600

    js arrow function

---
+++ problems/js_ES6/js_spread.js
@@ -0,0 +1,11 @@
+const q1 = ["jan","Feb","Mar"];
+const q2 = ["Apr","May","Jun"];
+const q3 = ["Jul","Aug","Sep"];
+const q4 = ["Oct","Nov","Dec"];
+
+const year = [...q1, ...q2, ...q3, ...q4];
+// console.log(year);
+
+const numbers = [23, 55, 21, 87, 56];
+let maxValue = Math.max(...numbers);
+console.log(maxValue);
