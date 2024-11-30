25951fa86c28464a989463e64b5493c331d3c1de
parvazreza00 <parvazreza00@gmail.com>
2024-12-01 01:22:51 +0600

    js object

---
+++ problems/js_ES6/js_default_para.js
@@ -0,0 +1,6 @@
+function greet(name, title="Mr."){
+    console.log(`Hello ${title} ${name}`);
+}
+
+greet("Jone Doe");
+greet("Toma","Miss.");
---
+++ problems/js_ES6/js_for_in_of.js
@@ -0,0 +1,19 @@
+const cities = ['Dhaka','bogra','Rajshahi','Rangpur'];
+
+const country ={
+    name:"bangladesh",
+    capital:"dhaka",
+}
+
+// for(item in cities){
+//     console.log(item+":"+cities[item]);
+// }
+
+
+for(key in country){
+    console.log(key+":"+country[key]);
+}
+
+// for(key of country){
+//     console.log(country[key]);
+// }
